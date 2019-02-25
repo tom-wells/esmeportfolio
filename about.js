69cf86f29558698e2c05b618e741046358d5e17c
Tom Wells <tom.wells89@gmail.com>
2019-02-25 17:00:39 +0000

    added resume

--- about.js
+++ about.js
@@ -4,11 +4,15 @@ const aboutSection = document.querySelector('section .about')
 const workTag = document.querySelector('.nav-links .work')
 const workSection = document.querySelector('.photos')
 
+const resumeTag = document.querySelector('.nav-links .my-resume')
+const resumeSection = document.querySelector('section .resume')
+
 const navTag = document.querySelector('nav')
 
 aboutTag.addEventListener('click', function(){
     aboutSection.classList.add('open-about')
     workSection.classList.add('close-work')
+    resumeSection.classList.remove('open-resume')
     navTag.classList.remove('open')
 
 })
@@ -16,6 +20,15 @@ aboutTag.addEventListener('click', function(){
 workTag.addEventListener('click', function(){
     aboutSection.classList.remove('open-about')
     workSection.classList.remove('close-work')
+    resumeSection.classList.remove('open-resume')
+    navTag.classList.remove('open')
+
+})
+
+resumeTag.addEventListener('click', function(){
+    aboutSection.classList.remove('open-about')
+    workSection.classList.add('close-work')
+    resumeSection.classList.add('open-resume')
     navTag.classList.remove('open')
 
 })
